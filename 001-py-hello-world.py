c6e5e49c28475a15cd3c0556008b149b33c7dd74
jwcha <neofanatic@gmail.com>
2018-07-16 02:03:26 -0700

    inital commit. completed first challenge

---
+++ 001-py-hello-world.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# HackerRank's Python challenges
+# 001. Hello World
+# Say "Hello, World!" With Python
+# input: You do not need to read any input in this challenge.
+# output: Print "Hello, World!" to stdout.
+# sample_output: Hello, World!
+
+
+print("Hello, World!")
---
+++ README.md
@@ -0,0 +1,9 @@
+# HackerRank-Python
+## author: jwcha
+## date created: 2018-07-16
+
+My solutions for HackerRank's Python challenges.
+Solutions will be numbered and named according to the URL of the challenge.
+
+#**TABLE OF CONTENTS**
+001. py-hello-world
